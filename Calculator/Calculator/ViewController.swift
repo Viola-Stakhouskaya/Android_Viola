2fa090ad60f50897cd5ecd5c16c0f0c73994436b
Viola Stakhouskaya <violastakhovskaya99@gmail.com>
2020-09-17 17:58:21 +0300

    start

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Viola on 9/17/20.
+//  Copyright © 2020 Viola Stakhouskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

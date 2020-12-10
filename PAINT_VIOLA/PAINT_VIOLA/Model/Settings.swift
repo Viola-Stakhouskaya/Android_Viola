a71891b3663618aa192a2f7a88ce7860f531f1a2
Viola Stakhouskaya <violastakhovskaya99@gmail.com>
2020-12-10 17:25:02 +0300

    paint_project_by_viola

---
+++ PAINT_VIOLA/PAINT_VIOLA/Model/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Paint
+//
+//  Created by Viola Stakhouskaya on 12/3/20.
+//
+
+import UIKit
+
+struct Settings {
+    
+    var tool: Tool
+    var fillColor: UIColor
+    var strokeColor: UIColor
+    var lineWidth: CGFloat
+    var opacity: CGFloat
+}
---
+++ PAINT_VIOLA/PAINT_VIOLA/Model/Shape.swift
@@ -0,0 +1,17 @@
+//
+//  Shape.swift
+//  Paint
+//
+//  Created by Viola Stakhouskaya on 11/29/20.
+//
+
+import Foundation
+
+enum Shape {
+    
+    case line
+    case ellipse
+    case circle
+    case rectangle
+    case square
+}
